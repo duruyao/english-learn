e1a853cd36399fc9aa7a1c64eda647e367449cd2
duruyao <duruyao@gmail.com>
2022-11-18 11:38:14 +0800

    chore: install requirements of the app

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 
 app_home="${PWD}"
 chmod +x ./*.py
+pip3 install -r requirements.txt
 pushd /usr/local/bin >/dev/null
 sudo rm -f trans youdao
 sudo ln -s "${app_home}"/trans.py trans
